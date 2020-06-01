87752c0577e16927877b85d88dfb5dc58ad57c55
Levon <5novel5@Mail.ru>
2020-06-01 13:31:31 +0300

    4 инд

---
+++ 4/ind4.pl
@@ -0,0 +1,17 @@
+ans:- penza_test(X),write(X),!.
+
+student(X):- X=sergey; X=boris; X=leonid; X=grigoriy; X=victor.
+goroda(Y):- Y=penza; Y=lvov; Y=moscow; Y=harkov; Y=riga.
+
+student_na(boris,penza).      %Борис родом из Риги, сделанном в Пензе.
+student_na(viktor,moscow).    %У Виктора велосипед из Москвы.
+student_na(grigoriy,harkov).  %У Григория велосипед из Харькова.
+student_na(sergey,riga).      %Сергей едет на велосипеде, сделанном в Риге.
+
+birth(boris,riga).       %Борис родом из Риги.
+birth(viktor,lvov).      %Виктор родом из Львова.
+
+velo(Person,City):-student(Person), goroda(City), student_na(Person,City),!;
+student(Person),goroda(City), not(student_na(Person,_)), not(student_na(_,City)).
+
+penza_test(Person):-student(Person), X=penza, not(birth(Person,_)), goroda(Unknown), not(Unknown=X),velo(Person,Unknown).
---
+++ 4/ind4.pl~
@@ -0,0 +1,11 @@
+student(X):- X=sergey; X=boris; X=leonid; X=grigoriy; X=victor.
+goroda(Y):- Y=penza; Y=lvov; Y=moscow; Y=harkov; Y=riga.
+
+student_na(boris,penza).      %Борис родом из Риги, сделанном в Пензе.
+student_na(viktor,moscow).    %У Виктора велосипед из Москвы.
+student_na(grigoriy,harkov).  %У Григория велосипед из Харькова.
+student_na(sergey,riga).      %Сергей едет на велосипеде, сделанном в Риге.
+
+birth(boris,riga).       %Борис родом из Риги.
+birth(viktor,lvov).      %Виктор родом из Львова.
+
